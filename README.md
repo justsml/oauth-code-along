344a83101e2297388c886dec00c935db574d1ef3
Dan Levy <dan@danlevy.net>
2018-02-28 16:10:14 -0700

    completed oauth lesson part 1/?

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 ### Objectives
 
 1. Define OAuth, Passport, Session
+  * OAuth: Method for authenticating users w/ 3rd party providers
+  * Passport: Auth for express
+  * Session: Place to store user info - on the server
 1. Research & Draw OAuth Diagram
 1. Setup Keys for one of: [Twitter](https://apps.twitter.com/app/new), [Github](https://github.com/settings/applications/new), [Facebook](https://developers.facebook.com/apps/), [Google](https://console.cloud.google.com/projectcreate).
 1. Setup [express bits](https://github.com/justsml/guides/blob/master/express/app.js)
--- index.js
+++ index.js
@@ -1,34 +1,91 @@
-const http        = require('http')
-const express     = require('express')
-const session     = require('express-session')
-const passport    = require('passport')
-const bodyParser  = require('body-parser')
-const morgan      = require('morgan')
-const cors        = require('cors')
-const app         = module.exports = express()
-const server      = http.createServer(app)
-const port        = parseInt(process.env.PORT || 3000)
-const devMode     = process.env.NODE_ENV !== 'production'
+const http            = require('http')
+const express         = require('express')
+const session         = require('express-session')
+const passport        = require('passport')
+const TwitterStrategy = require('passport-twitter').Strategy;
+const bodyParser      = require('body-parser')
+const morgan          = require('morgan')
+const cors            = require('cors')
+const app             = module.exports = express()
+const server          = http.createServer(app)
+const port            = parseInt(process.env.PORT || 3000)
+const devMode         = process.env.NODE_ENV !== 'production'
+require('dotenv').config()
+
+const users = []
+
+passport.use(new TwitterStrategy({
+    consumerKey: process.env.TWITTER_PUBLIC,
+    consumerSecret: process.env.TWITTER_SECRET,
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    console.log('AUTH HANDLED:', arguments)
+    let user = users.find(u => u.id === profile.id)
+    if (!user) {
+      users.push({id: profile.id, username: profile.username})
+    }
+    done(null, user)
+    // User.findOrCreate(..., function(err, user) {
+    //   if (err) { return done(err); }
+    //   done(null, user);
+    // });
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  console.log('serializeUser: ' + user._id)
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log('deserializeUser: ' + user._id)
+  let user = users.find(u => u.id === id)
+  if (!user) { done(new Error('User not found! ' + id))}
+  done(null, user)
+});
+
 
 app.set('view engine', 'pug')
-
-app.get('/', (req, res, next) => {
-  res.render('index', {user: req.user})
-})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan(devMode ? 'dev' : 'combined'))
 app.use(cors({origin: true}))
 app.use(session({
-  secret: 'keyboard cat',
+  secret: 'bquyqueajhbd',
   resave: true,
   saveUninitialized: true
 }));
 app.use(passport.initialize())
 app.use(passport.session())
 
+
+app.get('/', (req, res, next) => {
+  console.log('session:', req.session)
+  res.render('index', {user: req.user})
+})
+
+app.get('/login', (req, res, next) => {
+  console.log('session:', req.session)
+  res.render('index', {user: req.user})
+})
+
+
 // TODO: ADD (MOUNT) YOUR MIDDLEWARE (ROUTES) HERE
+// Redirect the user to Twitter for authentication.  When complete, Twitter
+// will redirect the user back to the application at
+//   /auth/twitter/callback
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+// Twitter will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { successRedirect: '/',
+                                     failureRedirect: '/login' }));
+
 // ^^^ Example: app.use('/v1/kitten', require('./routes/kitten'))
 // ^^^ Example: app.use('/cats', require('./routes/kitten'))
 
